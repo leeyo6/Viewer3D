38007c25fb628c2c440fb60a9f114d89ac134771
leeyo6 <leeyo6@gmail.com>
2020-04-16 18:16:30 -0400

    fx file chooser, close button works

--- Viewer3D/src/viewer3d/FXMLDocumentController.java
+++ Viewer3D/src/viewer3d/FXMLDocumentController.java
@@ -5,23 +5,46 @@
  */
 package viewer3d;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 /**
  *
  * @author leeyo
  */
 public class FXMLDocumentController implements Initializable {
+    @FXML private BorderPane ap;
+    
     
         @FXML
     private void handleMenuItemOpenAction(ActionEvent event) {
         System.out.println("Open");
-
+        FileChooser fileChooser = new FileChooser();
+        //fileChooser.setInitialDirectory(new File("data"));
+        //fileChooser.setInitialFileName("myfile.txt");
+        fileChooser.getExtensionFilters().addAll(
+         new FileChooser.ExtensionFilter("Obj Files", "*.obj")
+        ,new FileChooser.ExtensionFilter("csv Files", "*.csv")
+        );
+        
+        Stage stage = (Stage) ap.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null && selectedFile.exists()){
+            System.out.println(selectedFile.getPath());
+        }else{
+            System.out.println("File could not be opened.");
+        }
     }
 
     @FXML
@@ -39,6 +62,8 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleMenuItemCloseAction(ActionEvent event) {
         System.out.println("Close");
+        Stage stage = (Stage) ap.getScene().getWindow();
+        stage.close();
 
     }
     
@@ -46,5 +71,5 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
 }
